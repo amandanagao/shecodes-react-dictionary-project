2708d8512f8e518876c2610428ea5366f94de74e
amandanagao <nagaoamanda@gmail.com>
2022-11-30 11:22:10 -0300

    Added DictionarySearch Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import DictionarySearch from "./DictionarySearch";
+
 import "./App.css";
 
 export default function App() {
     return (
         <div className="App">
             <header className="App-header">Dictionary</header>
+            <DictionarySearch />
         </div>
     );
 }
